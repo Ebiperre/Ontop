2017d1a4e6783500a2b142f9b2716a32c722b97e
Travisken <kentravis37@gmail.com>
2024-02-23 13:45:53 +0100

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,26 @@ import { Routes, Route } from 'react-router-dom';
 import ErrorPage from './containers/404Page/404Page';
 import SignUpPageContainer from './containers/SignUpPageContainer/SignUpPageContainer';
 import SignInPageContainer from './containers/SignInPageContainer/SignInPageContainer';
+import HomePageContainer from './containers/HomePageContainer/HomePageContainer';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
   return (
-    <Routes>
+<>
+<Navbar />
+
+ <Routes>
       <Route path="*" element={<ErrorPage />} />
+<Route path="/home" element={<HomePageContainer />}/>
+
       <Route path="/sign-up" element={<SignUpPageContainer />} />
       <Route path="/sign-in" element={<SignInPageContainer />} />
     </Routes>
+
+</>
+
+   
   )
 }
 
--- src/containers/HomePageContainer/HomePageContainer.tsx
+++ src/containers/HomePageContainer/HomePageContainer.tsx
@@ -1,8 +1,44 @@
-import classes from './HomePageContainer.module.css'
+import classes from './HomePageContainer.module.css';
+import heroImage from "../../assets/images/hero_image.svg"
 
 const HomePageContainer = () => {
     return (
-        <div className={classes.container}>HomePageContainer</div>
+
+        <>
+
+            <main>
+
+                <section className={classes.hero_section}>
+                    <div className={classes.hero_text}>
+
+                        <h2>
+                            A Trusted and Secure Cryptocurrency Exchange
+                        </h2>
+
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae hic autem sit mollitia sunt illo nostrum harum quas iure facilis modi deserunt, voluptas earum. Tenetur vero distinctio voluptatum officiis repellendus.
+                        </p>
+
+                        <div className={classes.subscribe_field}>
+                            <div className={classes.subscribe_input}>
+                                <input type="email" placeholder="Input Email:" />
+
+                            </div>
+                            <button>
+                                Subscribe
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className={classes.hero_image}>
+                        <img src={heroImage} alt="" />
+                    </div>
+                </section>
+
+            </main>
+
+        </>
+
     )
 }
 
