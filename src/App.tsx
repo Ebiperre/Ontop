f054e2e49689ec3fbc82be590ff86e6d47b90bf0
franklyn101 <franklyninoma@gmail.com>
2024-02-28 13:35:22 +0100

    fixed merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 import ForgotPassword from './containers/ForgotPassword/ForgotPassword';
 import AboutPageContainer from './containers/AboutPageContainer/AboutPageContainer';
+import ServicePage from './pages/ServicePage';
 import ServicePageContainer from './containers/ServicePageContainer/ServicePageContainer';
 
 const router = createBrowserRouter([
@@ -32,7 +33,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/service",
-    element: <ServicePageContainer />
+    element: <ServicePage />
   },
   {
     path: "/home",
