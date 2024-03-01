24acc6ba29b4dab23a0913c0d599396c61a2a25b
Travisken <kentravis37@gmail.com>
2024-03-01 13:41:51 +0100

    push

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -87,10 +87,10 @@ export default function Navbar({ notNavlist, navBg }: NavBarProps) {
                     </svg>
                 </div>
 
-                <section className={click ? "absolute top-0 right-0 h-[100vh] w-[100vw] flex overflow-hidden" : "w-[0px] bg-purple"}>
+                <section className={click ? "fixed top-0 right-0 h-[100vh] w-[100vw] flex overflow-hidden" : "w-[0px] bg-purple"}>
                     <div onClick={closeNav} className="bg-[#0000003d]  flex-1"></div>
 
-                    <div className={click ? "w-3/4 overflow-hidden bg-white text-black pt-20 pb-4 px-2 flex justify-between flex-col" : "w-[0px]"}>
+                    <div className={click ? "w-3/4 overflow-hidden bg-white text-black py-20 px-2 flex justify-between flex-col" : "w-[0px]"}>
                         <ul className="flex flex-col items-start justify-start h-full w-full gap-8 overflow-hidden">
                             <Link to="/home">
                                 <li>Home</li>
