4b4b9348c00a785e3974773883b11582a0cb4595
franklyn101 <franklyninoma@gmail.com>
2024-03-06 14:53:24 +0100

    gitfcard

---
+++ src/pages/DashboardPages/DashBoardGiftCardPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DashboardLayout from '../../containers/DashboardLayout/DashboardLayout'
+import DashBoardGiftcardContainer from '../../containers/DashBoard/DashBoardGiftCardPageContainer/DashBoardGiftcardContainer'
+
+const DashBoardGiftCardPage = () => {
+  return (
+    <DashboardLayout>
+        <DashBoardGiftcardContainer />
+    </DashboardLayout>
+  )
+}
+
+export default DashBoardGiftCardPage
