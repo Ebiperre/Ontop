c6913c330f740d6e35f1daa95bef0d6f6458fd52
franklyn101 <franklyninoma@gmail.com>
2024-03-07 15:52:59 +0100

    addee context for statemanagement

--- src/containers/DashBoard/DashBoardGiftCardPageContainer/DashBoardGiftcardContainer.tsx
+++ src/containers/DashBoard/DashBoardGiftCardPageContainer/DashBoardGiftcardContainer.tsx
@@ -1,51 +1,8 @@
-import React from 'react'
-import { useState } from 'react';
-import { Link } from 'react-router-dom'; 
-import SteamImage from "../../../assets/images/card_steam.png"
-import AmazonImage from "../../../assets/images/card_amazon.png"
-import GoogleImage from "../../../assets/images/card_google.png"
-import AppleImage from "../../../assets/images/card_apple.png"
-import VisaImage from "../../../assets/images/card_visa.png"
-import RazerImage from "../../../assets/images/card_razer.png"
-import SephoraImage from "../../../assets/images/card_sephora.png"
+import React, { useContext } from 'react'
+import { AppContext } from '../../../context/AppContext';
 
-const DashBoardGiftcardContainer = () => {
-    const cards = [
-        {   
-            id: 1,
-            image: AmazonImage,
-            title: 'Amazon'
-        },
-        {
-            id: 2,
-            image: SteamImage,
-            title: 'Steam'
-        },
-        {
-            image: GoogleImage,
-            title: 'Google'
-        },
-        {
-            id: 3,
-            image: AppleImage,
-            title: 'Apple'
-        },
-        {
-            id: 4,
-            image: VisaImage,
-            title: 'Visa'
-        },
-        {
-            id: 5,
-            image: RazerImage,
-            title: 'Razer'
-        },
-        {
-            id: 6,
-            image: SephoraImage,
-            title: 'Sephora'
-        },
-    ]
+const DashBoardGiftcardContainer: React.FC = () => {
+const {cards} = useContext(AppContext)
    
   return (
     <div>
