e47f4e4b5d1a8ea3ac008d045a44d28d725f1a57
franklyn101 <franklyninoma@gmail.com>
2024-03-07 15:52:59 +0100

    addee context for statemanagement

--- src/utilities/giftcardUtilities.tsx
+++ src/utilities/giftcardUtilities.tsx
@@ -1,20 +1,20 @@
-import SteamImage from "../../../assets/images/card_steam.png"
-import AmazonImage from "../../../assets/images/card_amazon.png"
-import GoogleImage from "../../../assets/images/card_google.png"
-import AppleImage from "../../../assets/images/card_apple.png"
-import VisaImage from "../../../assets/images/card_visa.png"
-import RazerImage from "../../../assets/images/card_razer.png"
-import SephoraImage from "../../../assets/images/card_sephora.png"
+import SteamImage from "../../src/assets/images/card_steam.png"
+import AmazonImage from "../../src/assets/images/card_amazon.png"
+import GoogleImage from "../../src/assets/images/card_google.png"
+import AppleImage from "../../src/assets/images/card_apple.png"
+import VisaImage from "../../src/assets/images/card_visa.png"
+import RazerImage from "../../src/assets/images/card_razer.png"
+import SephoraImage from "../../src/assets/images/card_sephora.png"
 
 
-interface GiftCard {
+export type giftCardType = {
     id: number;
     image: string;
     title: string;
-}
+}[]
 
 
-const cards = [
+export const giftCards: giftCardType = [
     {   
         id: 1,
         image: AmazonImage,
@@ -24,10 +24,6 @@ const cards = [
         id: 2,
         image: SteamImage,
         title: 'Steam'
-    },
-    {
-        image: GoogleImage,
-        title: 'Google'
     },
     {
         id: 3,
@@ -41,12 +37,17 @@ const cards = [
     },
     {
         id: 5,
+        image: GoogleImage,
+        title: 'Google'
+    },
+    {
+        id: 6,
         image: RazerImage,
         title: 'Razer'
     },
     {
-        id: 6,
+        id: 7,
         image: SephoraImage,
         title: 'Sephora'
     },
-]
+];
