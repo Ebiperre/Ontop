09ffd90ac8abe1c280fa4c6a99d0936a6381b378
franklyn101 <franklyninoma@gmail.com>
2024-03-11 15:13:36 +0100

    added dynamic routing for each of the giftcards

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import AboutPage from './pages/AboutPage';
 import SignUpFormStepper from "./containers/SignUpPageContainer/SignUpFormStepper";
 import TermsAndConditionsPage from "./pages/TermsAndConditionsPage";
 import DashBoardHomePage from "./pages/DashboardPages/DashBoardHomePage";
+import DashBoardGiftCardPage from "./pages/DashboardPages/DashBoardGiftCardPage";
+import CardDetails from "./containers/DashBoard/DashBoardGiftCardPageContainer/CardDetails";
 
 function App() {
   return (
@@ -28,6 +30,8 @@ function App() {
         <Route path="/" element={<Navigate to="/home"></Navigate>} />
         <Route path="*" element={<ErrorPage />} />
         <Route path="/wallet-home" element={<DashBoardHomePage />} />
+        <Route path="/wallet-cards" element={<DashBoardGiftCardPage />} />
+        <Route path="/wallet-cards/:CardId" element={<CardDetails />} />
       </Routes>
     </Router>
   );
