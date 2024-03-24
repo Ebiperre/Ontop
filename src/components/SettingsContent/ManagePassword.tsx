112c930d2085d9ab83672838ffc25f66e490908d
kizitech <kizitoohani@gmail.com>
2024-03-24 07:42:41 +0100

    added the manage pin section

--- src/components/SettingsContent/ManagePassword.tsx
+++ src/components/SettingsContent/ManagePassword.tsx
@@ -30,74 +30,117 @@ function ManagePassword() {
 
   return (
     <>
-      <div className='text-left mb-10'>
-        <h2 className='font-medium text-lg md:text-2xl text-dark mb-2'>Manage password</h2>
-        <p className="text-gray-600">Keep your account secure. Update your password here.</p>
+      {/* Manage Pin */}
+      <div>
+        <div className='text-left mb-10'>
+          <h2 className='font-medium text-lg md:text-2xl text-dark mb-2'>Manage pin</h2>
+          <p className="text-gray-600">Keep your transaction secure. Update your transfer pin here.</p>
+        </div>
+        <Grid
+          templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+          gap={8}
+        >
+          <FormControl id="currentPin">
+            <FormLabel>Current Pin</FormLabel>
+            <Input
+              focusBorderColor="brand.dark"
+              type="password"
+              placeholder="Enter current pin"
+              onChange={() => { }}
+            />
+          </FormControl>
+          <FormControl id="newPin">
+            <FormLabel>New pin</FormLabel>
+            <Input
+              focusBorderColor="brand.dark"
+              type="password"
+              placeholder="Enter new pin"
+              onChange={() => { }}
+            />
+          </FormControl>
+          <FormControl id="confirmNewPin">
+            <FormLabel>Confirm New Pin</FormLabel>
+            <Input
+              focusBorderColor="brand.dark"
+              type="password"
+              placeholder="Confirm new pin"
+              onChange={() => { }}
+            />
+          </FormControl>
+        </Grid>
       </div>
-      <Grid
-        templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
-        gap={8}
-      >
-        <FormControl id="currentPassword">
-          <FormLabel>Current Password</FormLabel>
-          <Input
-            focusBorderColor="brand.dark"
-            type="password"
-            placeholder="Enter current password"
-            value={currentPassword}
-            onChange={(e) => setCurrentPassword(e.target.value)}
-          />
-        </FormControl>
-        <FormControl id="newPassword">
-          <FormLabel>New Password</FormLabel>
-          <Input
-            focusBorderColor="brand.dark"
-            type="password"
-            placeholder="Enter new password"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-          />
-        </FormControl>
-        <FormControl id="confirmNewPassword">
-          <FormLabel>Confirm New Password</FormLabel>
-          <Input
-            focusBorderColor="brand.dark"
-            type="password"
-            placeholder="Confirm new password"
-            value={confirmNewPassword}
-            onChange={(e) => setConfirmNewPassword(e.target.value)}
-          />
-        </FormControl>
 
-        <Modal isOpen={isModalOpen} onClose={closeModal}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Forgot Password?</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <p>Enter your email address to reset your password.</p>
-              <Input
-                type="email"
-                focusBorderColor="brand.dark"
-                placeholder="Enter your email address"
-              />
-            </ModalBody>
-            <ModalFooter>
-              <Button colorScheme="blue" mr={3}>
-                Reset Password
-              </Button>
-              <Button variant="ghost" onClick={closeModal}>Cancel</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </Grid>
-      <div className="flex flex-wrap mt-10 text-left">
-        <p className="text-sm text-gray-600 mb-2">
-          If you have forgotten your password, click the button below to reset it. Please provide your email address.
-        </p>
-        <Button variant={'link'} onClick={handleForgotPassword}>
-          Forgot Password?
-        </Button>
+      {/* Manage Password */}
+      <div>
+        <div className='text-left mt-16 mb-10'>
+          <h2 className='font-medium text-lg md:text-2xl text-dark mb-2'>Manage password</h2>
+          <p className="text-gray-600">Keep your account secure. Update your password here.</p>
+        </div>
+        <Grid
+          templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+          gap={8}
+        >
+          <FormControl id="currentPassword">
+            <FormLabel>Current Password</FormLabel>
+            <Input
+              focusBorderColor="brand.dark"
+              type="password"
+              placeholder="Enter current password"
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+            />
+          </FormControl>
+          <FormControl id="newPassword">
+            <FormLabel>New Password</FormLabel>
+            <Input
+              focusBorderColor="brand.dark"
+              type="password"
+              placeholder="Enter new password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+          </FormControl>
+          <FormControl id="confirmNewPassword">
+            <FormLabel>Confirm New Password</FormLabel>
+            <Input
+              focusBorderColor="brand.dark"
+              type="password"
+              placeholder="Confirm new password"
+              value={confirmNewPassword}
+              onChange={(e) => setConfirmNewPassword(e.target.value)}
+            />
+          </FormControl>
+
+          <Modal isOpen={isModalOpen} onClose={closeModal}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Forgot Password?</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <p>Enter your email address to reset your password.</p>
+                <Input
+                  type="email"
+                  focusBorderColor="brand.dark"
+                  placeholder="Enter your email address"
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3}>
+                  Reset Password
+                </Button>
+                <Button variant="ghost" onClick={closeModal}>Cancel</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </Grid>
+        <div className="flex flex-wrap mt-10 text-left">
+          <p className="text-sm text-gray-600 mb-2">
+            If you have forgotten your password, click the button below to reset it. Please provide your email address.
+          </p>
+          <Button variant={'link'} onClick={handleForgotPassword}>
+            Forgot Password?
+          </Button>
+        </div>
       </div>
     </>
   )
--- src/containers/DashBoard/DashboardPaymentPageContainer/DashboardPaymentPageContainer.tsx
+++ src/containers/DashBoard/DashboardPaymentPageContainer/DashboardPaymentPageContainer.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BiSolidCreditCardAlt } from 'react-icons/bi'
 
 const DashboardPaymentPageContainer: React.FC = () => {
+    const [showAddCardForm, setShowAddCardForm] = useState(false);
     return (
         <div className="px-4 lg:px-8 pt-14 pb-10">
             <div className=" flex flex-col gap-16 relative">
@@ -11,43 +13,48 @@ const DashboardPaymentPageContainer: React.FC = () => {
                             Stay on top of your payments. Monitor, analyze, and manage your transactions seamlessly with our intuitive platform.
                         </p>
                     </div>
-                    <button onClick={() => { }} className="bg-orange2 text-white px-5 py-2.5 rounded-lg font-medium hover:bg-blue-100">Add Card</button>
+                    <button onClick={() => setShowAddCardForm(true)} className="bg-orange2 text-white px-5 py-2.5 rounded-lg font-medium hover:bg-orange flex items-center gap-1">
+                        <BiSolidCreditCardAlt size={30} />
+                        <span>Add Card</span>
+                    </button>
                 </div>
                 <div>
-                    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-100">
-                        <form className="bg-white w-full max-w-3xl mx-auto px-6 py-8 shadow-md rounded-md flex">
-                            <div className="w-1/2 pr-8 border-r-2 border-slate-300">
-                                <label className="text-neutral-800 font-bold text-sm mb-2 block">Card number:</label>
-                                <input type="text" className="flex h-10 w-full rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mb-4" />
-                                <div className="flex gap-x-2 mb-4">
-                                    <div className="flex-1">
-                                        <label className="text-neutral-800 font-bold text-sm mb-2 block">Exp. date:</label>
-                                        <input type="text" className="flex h-10 w-full rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mb-4" />
+                    {showAddCardForm && (
+                        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-100">
+                            <form className="bg-white w-full max-w-3xl mx-auto px-6 py-8 shadow-md rounded-md flex">
+                                <div className="w-1/2 pr-8 border-r-2 border-slate-300">
+                                    <label className="text-neutral-800 font-bold text-sm mb-2 block">Card number:</label>
+                                    <input type="text" className="flex h-10 w-full rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mb-4" />
+                                    <div className="flex gap-x-2 mb-4">
+                                        <div className="flex-1">
+                                            <label className="text-neutral-800 font-bold text-sm mb-2 block">Exp. date:</label>
+                                            <input type="text" className="flex h-10 w-full rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mb-4" />
+                                        </div>
+                                        <div className="flex-1">
+                                            <label className="text-neutral-800 font-bold text-sm mb-2 block">CCV:</label>
+                                            <input type="text" className="flex h-10 w-full rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mb-4" />
+                                        </div>
                                     </div>
-                                    <div className="flex-1">
-                                        <label className="text-neutral-800 font-bold text-sm mb-2 block">CCV:</label>
-                                        <input type="text" className="flex h-10 w-full rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mb-4" />
-                                    </div>
-                                </div>
 
-                                <label className="text-neutral-800 font-bold text-sm mb-2 block">Card holder:</label>
-                                <input type="text" className="flex h-10 w-full rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" />
-                            </div>
-                            <div className="w-1/2 pl-8">
-                                <div className="w-full h-56" style={{ perspective: '1000px' }}>
-                                    <div className="crediCard relative cursor-pointer transition-transform duration-500" style={{ transformStyle: 'preserve-3d' }}>
-                                        <div className="w-full m-auto rounded-xl shadow-2xl absolute" style={{ backfaceVisibility: 'hidden' }}>
-                                            <img src="https://i.ibb.co/B2vQ0xG/Card-1.jpg" className="object-cover w-full h-full" />
-                                        </div>
-                                        <div className="w-full m-auto rounded-xl shadow-2xl absolute" style={{ backfaceVisibility: 'hidden', transform: 'rotateY(180deg)' }}>
-                                            <img src="https://i.ibb.co/ThGc8mn/Card-2.jpg" className="object-cover w-full h-full" />
+                                    <label className="text-neutral-800 font-bold text-sm mb-2 block">Card holder:</label>
+                                    <input type="text" className="flex h-10 w-full rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" />
+                                </div>
+                                <div className="w-1/2 pl-8">
+                                    <div className="w-full h-56" style={{ perspective: '1000px' }}>
+                                        <div className="crediCard relative cursor-pointer transition-transform duration-500" style={{ transformStyle: 'preserve-3d' }}>
+                                            <div className="w-full m-auto rounded-xl shadow-2xl absolute" style={{ backfaceVisibility: 'hidden' }}>
+                                                <img src="https://i.ibb.co/B2vQ0xG/Card-1.jpg" className="object-cover w-full h-full" />
+                                            </div>
+                                            <div className="w-full m-auto rounded-xl shadow-2xl absolute" style={{ backfaceVisibility: 'hidden', transform: 'rotateY(180deg)' }}>
+                                                <img src="https://i.ibb.co/ThGc8mn/Card-2.jpg" className="object-cover w-full h-full" />
+                                            </div>
                                         </div>
                                     </div>
+                                    <p className="text-center text-neutral-800 font-bold text-sm mb-2 block">Hover the card</p>
                                 </div>
-                                <p className="text-center text-neutral-800 font-bold text-sm mb-2 block">Hover the card</p>
-                            </div>
-                        </form>
-                    </main>
+                            </form>
+                        </main>
+                    )}
                 </div>
             </div>
         </div>
