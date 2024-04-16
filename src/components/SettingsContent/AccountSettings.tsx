211dc878bfabebbfd201d1051b754b711c29c184
kizitech <kizitoohani@gmail.com>
2024-04-16 16:19:40 +0100

    added the billing information edit section to the account settings

--- src/components/SettingsContent/AccountSettings.tsx
+++ src/components/SettingsContent/AccountSettings.tsx
@@ -5,7 +5,7 @@ function AccountSettings() {
     <>
       <div className='text-left mb-10'>
         <h2 className='font-medium text-lg md:text-2xl text-dark mb-2'>Contact Information</h2>
-        <p className="text-gray-600">Tell us about yourself so we can know you better. <br />Share more information with us if you’d like.</p>
+        <p className="text-gray-600">Tell us about yourself so we can know you better. Share more information with us if you’d like.</p>
       </div>
       <Grid
         templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
@@ -68,6 +68,27 @@ function AccountSettings() {
           />
         </FormControl>
       </Grid>
+      <div className="space-y-5 mt-20">
+        <div className='text-left mb-10'>
+          <h2 className='font-medium text-lg md:text-2xl text-dark mb-2'>Billing Address</h2>
+          <p className="text-gray-600">Please review and make any necessary edits or changes to your billing address details below.</p>
+        </div>
+
+        <div className='flex flex-col gap-5'>
+          <input id="hs-pro-dacmba" type="text" className="py-3 vrf2y block w-full d1k81 nq4w8 dtjcu ggbym input-focus kko9e tk4f7" placeholder="Street address" />
+          <input type="text" className="py-3 vrf2y block w-full d1k81 nq4w8 dtjcu ggbym input-focus kko9e tk4f7" placeholder="Apt or suite number" />
+          <input type="text" className="py-3 vrf2y block w-full d1k81 nq4w8 dtjcu ggbym input-focus kko9e tk4f7" placeholder="City" />
+
+          <div className="grid zvyci gap-5">
+            <div>
+              <input type="text" className="py-3 vrf2y block w-full d1k81 nq4w8 dtjcu ggbym input-focus kko9e tk4f7" placeholder="State" />
+            </div>
+            <div>
+              <input type="text" className="py-3 vrf2y block w-full d1k81 nq4w8 dtjcu ggbym input-focus kko9e tk4f7" placeholder="ZIP code" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
