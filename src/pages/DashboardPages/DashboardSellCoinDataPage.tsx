ad44b2753d4d4a4d07b446bedcdb554f9e7b477f
franklyn101 <franklyninoma@gmail.com>
2024-04-22 13:55:24 +0100

    fixed padding

--- src/pages/DashboardPages/DashboardSellCoinDataPage.tsx
+++ src/pages/DashboardPages/DashboardSellCoinDataPage.tsx
@@ -2,8 +2,6 @@ import DashboardLayout from "../../containers/DashBoard/DashboardLayout/Dashboar
 import SellCoinDataContianer from "../../containers/DashBoard/DashboardSellPageContainer/SellCoinDataContainer";
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 export default function DashboardSellCoinPages() {
 
   return (
@@ -12,21 +10,5 @@ export default function DashboardSellCoinPages() {
     </DashboardLayout>
   );
 }
-=======
-=======
->>>>>>> f3d4015833621b8c3f1ee1b19437e6bfc99c527d
-const DashboardSellCoinPages = () =>{
 
-    return (
-      <DashboardLayout>
-        <SellCoinDataContianer />
-      </DashboardLayout>
-    );
-  };
-  export default  DashboardSellCoinPages
-<<<<<<< HEAD
-  
->>>>>>> 83f5873 (added layout)
-=======
-  
->>>>>>> f3d4015833621b8c3f1ee1b19437e6bfc99c527d
+
