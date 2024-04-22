0f576e4ab76b3ea49380439300d260a34d2d39d6
franklyn101 <franklyninoma@gmail.com>
2024-04-22 13:41:56 +0100

    added the correct layout

--- src/containers/DashBoardContainers/DashBoardGiftCardPageContainer/CardDetails.tsx
+++ src/containers/DashBoardContainers/DashBoardGiftCardPageContainer/CardDetails.tsx
@@ -51,10 +51,13 @@ const CardDetails = () => {
 
 
   return (
+<<<<<<< HEAD:src/containers/DashBoardContainers/DashBoardGiftCardPageContainer/CardDetails.tsx
     <div>
       <div className='w-[100%]'>
      
       </div>
+=======
+>>>>>>> 3e8d8fa (added the correct layout):src/containers/DashBoard/DashBoardGiftCardPageContainer/CardDetails.tsx
     <div className='px-6 xl:px-60 xl:pb-8'>
       <div className='flex justify-between items-center pt-2'>
         <div className='flex justify-center items-center gap-3'>
@@ -62,12 +65,15 @@ const CardDetails = () => {
           <h1 className='text-xl font-medium'>{activeCard?.title}</h1>
         </div>
         <p className="font-medium cursor-pointer xl:text-lg" onClick={() => { navigate('/dashboard/cards') }} >Back</p>
+<<<<<<< HEAD:src/containers/DashBoardContainers/DashBoardGiftCardPageContainer/CardDetails.tsx
       <div className='flex justify-between items-center xl:flex xl:justify-between xl:items-center pt-2'>
         <div className='flex justify-center items-center gap-3 mt-24'>
       <img className='w-12 xl:w-20' src={activeCard?.image} alt="" />
       <h1 className='text-xl font-medium'>{activeCard?.title}</h1>
       </div>
       <p className="font-medium cursor-pointer mt-20 xl:text-lg" onClick={() => {navigate('/dashboard-cards')}} >Back</p>
+=======
+>>>>>>> 3e8d8fa (added the correct layout):src/containers/DashBoard/DashBoardGiftCardPageContainer/CardDetails.tsx
       </div>
       <div className="flex flex-col items-start justify-start pt-4">
         <p className='text-xl'>Fill the form to start trading </p>
@@ -109,8 +115,8 @@ const CardDetails = () => {
       <button className='w-full bg-purple2 mt-8 text-white py-4 rounded-md xl:py-2 xl:mr-[50rem] xl:text-xl xl:w-36'>Confirm Trade</button>
 
     </div>
-    </div>
 
   )
 }
 
+export default CardDetails;
